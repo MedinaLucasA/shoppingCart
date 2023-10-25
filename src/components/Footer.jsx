4628850b568784452d0492f3da08e9c41da73412
MiliTrres <milagros.torres.42159@gmail.com>
2023-10-24 21:08:13 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h4>Shopping Cart con React ⚛️ －</h4>
+      <h5>(useContext & useReducer)</h5>
+    </footer>
+  )
+}
